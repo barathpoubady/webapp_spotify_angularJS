96135f260ac26ca6519711c4e968125283cf02b1
POUBADY <barath.poubady@capgemini.com>
2016-09-27 15:05:18 +0200

    ajout de AngularJS avec Yoeman command yo angular

---
+++ app/scripts/controllers/about.js
@@ -0,0 +1,17 @@
+'use strict';
+
+/**
+ * @ngdoc function
+ * @name webappSpotifyAngularJsApp.controller:AboutCtrl
+ * @description
+ * # AboutCtrl
+ * Controller of the webappSpotifyAngularJsApp
+ */
+angular.module('webappSpotifyAngularJsApp')
+  .controller('AboutCtrl', function () {
+    this.awesomeThings = [
+      'HTML5 Boilerplate',
+      'AngularJS',
+      'Karma'
+    ];
+  });
---
+++ app/scripts/controllers/main.js
@@ -0,0 +1,17 @@
+'use strict';
+
+/**
+ * @ngdoc function
+ * @name webappSpotifyAngularJsApp.controller:MainCtrl
+ * @description
+ * # MainCtrl
+ * Controller of the webappSpotifyAngularJsApp
+ */
+angular.module('webappSpotifyAngularJsApp')
+  .controller('MainCtrl', function () {
+    this.awesomeThings = [
+      'HTML5 Boilerplate',
+      'AngularJS',
+      'Karma'
+    ];
+  });
