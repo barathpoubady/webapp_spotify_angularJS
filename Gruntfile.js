720ec17328e4bf2728ec717c75bbb9fd3fc2a3b7
barathpoubady <poubady.barath@gmail.com>
2016-10-15 23:39:48 +0200

    test 2 - generate requireJs

--- Gruntfile.js
+++ Gruntfile.js
@@ -451,6 +451,8 @@ module.exports = function (grunt) {
     ]);
   });
 
+  grunt.loadNpmTasks('grunt-bower-requirejs');
+
   grunt.registerTask('server', 'DEPRECATED TASK. Use the "serve" task instead', function (target) {
     grunt.log.warn('The `server` task has been deprecated. Use `grunt serve` to start a server.');
     grunt.task.run(['serve:' + target]);
@@ -487,6 +489,7 @@ module.exports = function (grunt) {
     'newer:jshint',
     'newer:jscs',
     'test',
-    'build'
+    'build',
+    'bower'
   ]);
 };
